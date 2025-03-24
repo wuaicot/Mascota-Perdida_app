4ed2509d787ef1740be57a3eae21ad6ce3f189ea
wuaicot <wuaicot@gmail.com>
2025-03-24 06:42:37 -0300

    deployed

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true
 });
 
